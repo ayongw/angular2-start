19f73f4de1775d7e9c2ad25eb6c51d497ae953a1
John Jiang <jiangguangtao@foxmail.com>
2016-06-25 17:00:23 +0800

    first commit of angular2 start up

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App <small>is start up</small></h1>'
+})
+export class AppComponent { }
