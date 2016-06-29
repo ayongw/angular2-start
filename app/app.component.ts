b4a4b97a0d2c97df7667e168cd8ad35c80b4813a
John Jiang <jiangguangtao@foxmail.com>
2016-06-29 22:02:28 +0800

    using service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 import {Hero} from "./hero";
 import {HeroDetailComponent} from './hero-detail.component'
+import {HeroService} from './hero.service'
 
 @Component({
   selector: 'my-app',
@@ -63,30 +65,33 @@ import {HeroDetailComponent} from './hero-detail.component'
     border-radius: 4px 0 0 4px;
   }
   `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers : [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  /**
+   * 用于注入服务的入口
+   */
+  constructor(private heroService:HeroService) {
+    
+  }
+  
   title = "Tour of Heroes";
-  heroes = HEROES;
+  heroes:Hero[];
 
   selectedHero:Hero;
 
   onSelect = function (hero:Hero) {
     this.selectedHero = hero;
   }
+  /**
+   * init method
+   */
+  ngOnInit() {
+    this.initHeroList();
+  }
+  private initHeroList() {
+    this.heroes = this.heroService.getHeros();
+  }
 }
 
-
-const HEROES:Hero[] = [
-  {id: 10, name: 'Mr Nice'},
-  {id: 11, name: 'John Jiang'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
---
+++ app/hero.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import {HEROES} from './mock.heros'
+
+@Injectable()
+export class HeroService {
+    // begin
+    getHeros() {
+        return HEROES;
+    }
+}
