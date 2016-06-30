39fd5f737be796c26cad11f6700e5af112c78b54
Jiang Guangtao <jiangguangtao@chexiangjia.com>
2016-06-30 15:39:36 +0800

    before routing

---
+++ app/heroes.component.ts
@@ -0,0 +1,96 @@
+import {Component, OnInit} from '@angular/core';
+
+import {Hero} from "./hero";
+import {HeroDetailComponent} from './hero-detail.component'
+import {HeroService} from './hero.service'
+
+@Component({
+  selector: 'my-heroes',
+  template: `
+    <hero-detail [hero]="selectedHero"></hero-detail>
+    <h2>My heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+        <span class="badge">{{hero.id}}</span>{{hero.name}}
+      </li>
+    </ul>
+  `,
+  styles: [`
+    .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+  `],
+  directives: [HeroDetailComponent]
+})
+export class HeroesComponent implements OnInit {
+  /**
+   * 用于注入服务的入口
+   */
+  constructor(private heroService:HeroService) {
+
+  }
+  heroes:Hero[];
+
+  selectedHero:Hero;
+
+  onSelect(hero:Hero) {
+    this.selectedHero = hero;
+  };
+
+  /**
+   * init method
+   */
+  ngOnInit() {
+    this.initHeroList();
+  }
+
+  private initHeroList() {
+    // this.heroes = this.heroService.getHeros();
+    this.heroService.getHeros().then(heros => this.heroes = heros);
+  }
+}
+
